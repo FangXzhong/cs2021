a487659ac2878fd5fa2d75f138b57f2265a1cfbc
FangXzhong <fangxz2002@outlook.com>
2021-09-30 14:44:39 +0800

    2021.9.30
    Signed-off-by: FangXzhong <fangxz2002@outlook.com>

--- homework/2/1221A. 2048 Game.py
+++ homework/2/1221A. 2048 Game.py
@@ -1,1 +1,20 @@
-
+for i in range(int(input())):
+    flag = 0
+    num = int(input())
+    numbers = [int(i) for i in input().split(" ")]
+    if 2048 in numbers:
+        print("YES")
+        continue
+    max_label = max(numbers, key=numbers.count)
+    while numbers.count(max_label) > 1:
+        numbers.remove(max_label)
+        numbers.remove(max_label)
+        numbers.append(max_label * 2)
+        if 2048 in numbers:
+            flag = 1
+            break
+        max_label = max(numbers, key=numbers.count)
+    if flag:
+        print("YES")
+    else:
+        print("NO")
