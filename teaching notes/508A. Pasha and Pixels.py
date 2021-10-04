c175f8e9e762d2886d2c0f3a60c2ae940d963b7b
FangXzhong <fangxz2002@outlook.com>
2021-10-04 15:55:56 +0800

    2021.10.4 这道题对我来说还是挺有挑战性的
    Signed-off-by: FangXzhong <fangxz2002@outlook.com>

--- teaching notes/508A. Pasha and Pixels.py
+++ teaching notes/508A. Pasha and Pixels.py
@@ -1,1 +1,20 @@
+"""
+这道题对我来说还是挺有挑战性的。
+其实这道题的思路我还是比较清晰的，最烦的是下标的访问问题。
+包括刚开始我的程序因为嵌套了两个大循环超时了，后来改成2*2的循环
+"""
 
+n, m, k = [int(i) for i in input().split(" ")]
+picture = [[0 for i in range(n)] for j in range(m)]
+for i in range(k):
+    x, y = [int(j) for j in input().split(" ")]
+    if picture[y - 1][x - 1] == 1:
+        continue
+    picture[y - 1][x - 1] = 1
+    if i >= 3:
+        for a in range(max(x-2, 0), min(x, n-1)):
+            for b in range(max(y-2, 0), min(y, m-1)):
+                if picture[b][a] == picture[b + 1][a] == picture[b][a + 1] == picture[b + 1][a + 1] == 1:
+                    print(i + 1)
+                    quit()
+print(0)
