9558bc12108b65ef27697fac7a453ae09891c117
FangXzhong <fangxz2002@outlook.com>
2021-10-09 16:21:36 +0800

    C++yyds 2021.10.9
    Signed-off-by: FangXzhong <fangxz2002@outlook.com>

--- teaching notes/1425A. Arena of Greed.py
+++ teaching notes/1425A. Arena of Greed.py
@@ -1,18 +1,71 @@
-# def deal1(coin):
-#     score = 0
-#     if coin == 0:
-#         return 0, 0
-#     if coin % 2 == 0:
-#         coin /= 2
-#         score += coin + deal1(deal1(coin)[0])[1]
-#         return coin, score
-#     else:
-#         coin -= 1
-#         score += 1 + deal1(deal1(coin)[0])[1]
-#         return coin, score
-#
-#
-# for i in range(int(input())):
-#     print(int(deal1(int(input()))[1]))
-# 上面这个能用，但是超时了，主要问题估计是递归在层数较多的时候复杂度太高
+# C++解决的
 
+# # include <bits/stdc++.h>
+# using
+# namespace
+# std;
+# typedef
+# long
+# long
+# ll;
+# const
+# int
+# maxn = 4e5 + 7;
+# const
+# int
+# mod = 1e9 + 7;
+# char
+# x;
+# ll
+# n, t, s;
+# int
+# main()
+# {
+#     cin >> t;
+# while (t - -)
+#     {
+#         cin >> n;
+#     s = 0;
+#     int
+#     k = 0;
+#     while (n)
+#         {
+#         if (k % 2 == 0)
+#         {
+#         if (n % 2 == 1)
+#         {
+#             s + +;
+#         n - -;
+#         }
+#         else
+#         {
+#         if ((n / 2) % 2 == 0 & & n > 4)
+#         {
+#             s + +;
+#         n - -;
+#         }
+#         else
+#         {
+#             s += n / 2;
+#         n /= 2;
+#         }
+#         }
+#         }
+#         else
+#         {
+#         if (n % 2 == 1)
+#         n - -;
+#         else if ((n / 2) % 2 == 0 & & n > 4)
+#         {
+#         n--;
+#         }
+#         else
+#         n /= 2;
+#
+#         }
+#         k++;
+#         }
+#         cout << s << endl;
+#     }
+#     return 0;
+# }
