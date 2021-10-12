e7d048cee3ffd8a407ef80dd231b3cdbfbb723f4
ipad <fangxzh@pku.edu.cn>
2021-10-12 09:46:11 +0800

    应该没问题了，还没试过

--- teaching notes/580C. Kefa and Park.py
+++ teaching notes/580C. Kefa and Park.py
@@ -1,6 +1,6 @@
 n, m = [int(i) for i in input().split(" ")]
 cost = [int(i) for i in input().split(" ")]
-result = [0]*n # 用于存储最终节点消耗的列表，索引号=节点号-1，如[1,2,0,0]
+result = [0,]*n # 用于存储最终节点消耗的列表，索引号=节点号-1，如[1,2,0,0]
 nodes = dict() # 用于存储节点关系的字典，如1能到2和3，则{1:[2,3]}
 money = m - cost[0]
 result[0] = cost[0]
@@ -15,5 +15,6 @@ while nodes:
     selected_node = min(nodes.keys())
     for item in nodes[selected_node]:
         result[item - 1] = cost[selected_node - 1] + cost[item - 1]
+    print(result)
     del nodes[selected_node] 
 print(result)
