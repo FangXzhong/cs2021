940b2f96fd40327a85344411a1d43adce037985a
FangXzhong <fangxz2002@outlook.com>
2021-10-12 17:28:24 +0800

    完成第四次作业 2021.10.12
    Signed-off-by: FangXzhong <fangxz2002@outlook.com>

--- teaching notes/580C. Kefa and Park.py
+++ teaching notes/580C. Kefa and Park.py
@@ -1,7 +1,7 @@
 n, m = [int(i) for i in input().split(" ")]
 cost = [int(i) for i in input().split(" ")]
-result = [0,]*n # 用于存储最终节点消耗的列表，索引号=节点号-1，如[1,2,0,0]
-nodes = dict() # 用于存储节点关系的字典，如1能到2和3，则{1:[2,3]}
+result = [0, ] * n  # 用于存储最终节点消耗的列表，索引号=节点号-1，如[1,2,0,0]
+nodes = dict()  # 用于存储节点关系的字典，如1能到2和3，则{1:[2,3]}
 money = m - cost[0]
 result[0] = cost[0]
 for i in range(n - 1):
@@ -16,5 +16,5 @@ while nodes:
     for item in nodes[selected_node]:
         result[item - 1] = result[selected_node - 1] + cost[item - 1]
     print(result)
-    del nodes[selected_node] 
+    del nodes[selected_node]
 print(result)
