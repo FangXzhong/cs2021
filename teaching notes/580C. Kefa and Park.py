80cf4d806d6efb7f08f96e70baa1c077e6573c80
FangXzhong <fangxz2002@outlook.com>
2021-10-14 20:31:21 +0800

    这道题的样例我还是有点没理解，，，，再看看 2021.10.14
    Signed-off-by: FangXzhong <fangxz2002@outlook.com>

--- teaching notes/580C. Kefa and Park.py
+++ teaching notes/580C. Kefa and Park.py
@@ -2,10 +2,12 @@ n, m = [int(i) for i in input().split(" ")]
 cost = [int(i) for i in input().split(" ")]
 result = [0, ] * n  # 用于存储最终节点消耗的列表，索引号=节点号-1，如[1,2,0,0]
 nodes = dict()  # 用于存储节点关系的字典，如1能到2和3，则{1:[2,3]}
-money = m - cost[0]
+money = m
 result[0] = cost[0]
+not_leave_node = set()  # 用于统计哪些节点是叶子节点
 for i in range(n - 1):
     x, y = [int(i) for i in input().split(" ")]
+    not_leave_node.add(x)
     if x in nodes:
         nodes[x].append(y)
     else:
@@ -15,6 +17,10 @@ while nodes:
     selected_node = min(nodes.keys())
     for item in nodes[selected_node]:
         result[item - 1] = result[selected_node - 1] + cost[item - 1]
-    print(result)
     del nodes[selected_node]
+
+total_set = set(list(range(1, n + 1)))
+leave_node = total_set - not_leave_node
+print(leave_node)
 print(result)
+print(sum(1 if result[i-1] <= money else 0 for i in leave_node))
