f0a55a259335719652706880f8a36fc36943245a
FangXzhong <fangxz2002@outlook.com>
2021-10-26 23:51:02 +0800

    19949 加了注释
    Signed-off-by: FangXzhong <fangxz2002@outlook.com>

--- homework/6/19949：提取实体v0.2.py
+++ homework/6/19949：提取实体v0.2.py
@@ -3,9 +3,10 @@ sentences = list()
 result = 0
 for i in range(int(input())):
     sentences.append(input())
-flag = 0
+
+flag = 0  # 这个变量用来指示连续的主体所包含的单词索引终止位置
 for item in sentences:
-    skip_time = 0
+    skip_time = 0  # 如果一个主体包含了多个单词，那么就需要跳过后续所有的单个单词
     item_split = item.split(' ')
     for j in range(len(item_split)):
         if skip_time > 0:
