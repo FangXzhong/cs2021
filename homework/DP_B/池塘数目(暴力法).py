ff308009ddfac8823a20693e5f65f3ca42f0e6f4
Froza <fangxz2019@163.com>
2021-12-05 16:37:44 +0800

    暴力遍历测试样例能过，但是始终都是WA，DFS能过。 我还是没有想通为什么暴力遍历出错了
    Signed-off-by: Froza <fangxz2019@163.com>

--- homework/DP_B/池塘数目(暴力法).py
+++ homework/DP_B/池塘数目(暴力法).py
@@ -1,5 +1,116 @@
+# # 过不了，不知道是为什么
+# case_num = int(input().strip())
+# for case in range(case_num):
+#     n, m = [int(i) for i in input().strip().split(" ")]
+#     mymap = []
+#     for line in range(n):
+#         mymap.append(list(input().strip()))
+#     result = 0
+#     checked = set()
+#     if n == 1:
+#         if m == 1:
+#             if mymap[0][0] == "W":
+#                 print(1)
+#                 continue
+#             else:
+#                 print(0)
+#                 continue
+#         else:
+#             for j in range(m):
+#                 if mymap[0][j] == "W":
+#                     if j == 0:
+#                         neighbors = [mymap[0][1], ]
+#                         checked.add((0, 1))
+#                     elif j == m - 1:
+#                         neighbors = [mymap[0][m - 2], ]
+#                         checked.add((0, m - 2))
+#                     else:
+#                         neighbors = [mymap[0][j - 1], mymap[0][j + 1]]
+#                         checked.add((0, j - 1))
+#                         checked.add((0, j + 1))
+#                     if (0, j) not in checked:
+#                         result += 1
+#     else:
+#         for i in range(n):
+#             for j in range(m):
+#                 if mymap[i][j] == 'W':
+#                     neighbors = []
+#                     if i == 0:
+#                         if j == 0:
+#                             neighbors = [mymap[i][j + 1], mymap[i + 1][j], mymap[i + 1][j + 1]]
+#                             checked.add((i, j + 1))
+#                             checked.add((i + 1, j))
+#                             checked.add((i + 1, j + 1))
+#                         elif j == m - 1:
+#                             neighbors = [mymap[i][j - 1], mymap[i + 1][j], mymap[i + 1][j - 1]]
+#                             checked.add((i, j - 1))
+#                             checked.add((i + 1, j))
+#                             checked.add((i + 1, j - 1))
+#                         else:
+#                             neighbors = [mymap[i][j + 1], mymap[i][j - 1], mymap[i + 1][j], mymap[i + 1][j - 1], mymap[i + 1][j + 1]]
+#                             checked.add((i, j + 1))
+#                             checked.add((i, j - 1))
+#                             checked.add((i + 1, j + 1))
+#                             checked.add((i + 1, j - 1))
+#                             checked.add((i + 1, j))
+#                     elif i == n - 1:
+#                         if j == 0:
+#                             neighbors = [mymap[i][j + 1], mymap[i - 1][j], mymap[i - 1][j + 1]]
+#                             checked.add((i, j + 1))
+#                             checked.add((i - 1, j))
+#                             checked.add((i - 1, j + 1))
+#                         elif j == m - 1:
+#                             neighbors = [mymap[i][j - 1], mymap[i - 1][j], mymap[i - 1][j - 1]]
+#                             checked.add((i, j - 1))
+#                             checked.add((i - 1, j))
+#                             checked.add((i - 1, j - 1))
+#                         else:
+#                             neighbors = [mymap[i][j + 1], mymap[i][j - 1], mymap[i - 1][j], mymap[i - 1][j - 1],
+#                                          mymap[i - 1][j + 1]]
+#                             checked.add((i, j + 1))
+#                             checked.add((i, j - 1))
+#                             checked.add((i - 1, j + 1))
+#                             checked.add((i - 1, j - 1))
+#                             checked.add((i - 1, j))
+#                     else:
+#                         if j == 0:
+#                             neighbors = [mymap[i][j + 1], mymap[i - 1][j], mymap[i + 1][j], mymap[i - 1][j + 1], mymap[i + 1][j + 1]]
+#                             checked.add((i, j + 1))
+#                             checked.add((i - 1, j))
+#                             checked.add((i + 1, j))
+#                             checked.add((i - 1, j + 1))
+#                             checked.add((i + 1, j + 1))
+#                         elif j == m - 1:
+#                             neighbors = [mymap[i][j - 1], mymap[i - 1][j], mymap[i + 1][j], mymap[i - 1][j - 1],
+#                                          mymap[i + 1][j - 1]]
+#                             checked.add((i, j - 1))
+#                             checked.add((i - 1, j))
+#                             checked.add((i + 1, j))
+#                             checked.add((i - 1, j - 1))
+#                             checked.add((i + 1, j - 1))
+#                         else:
+#                             neighbors = [mymap[i][j - 1], mymap[i - 1][j], mymap[i + 1][j], mymap[i][j + 1], mymap[i - 1][j - 1],
+#                                          mymap[i + 1][j - 1], mymap[i + 1][j + 1], mymap[i - 1][j + 1]]
+#                             checked.add((i, j - 1))
+#                             checked.add((i, j + 1))
+#                             checked.add((i - 1, j))
+#                             checked.add((i + 1, j))
+#                             checked.add((i - 1, j - 1))
+#                             checked.add((i - 1, j + 1))
+#                             checked.add((i + 1, j - 1))
+#                             checked.add((i + 1, j + 1))
+#                     if (i, j) not in checked:
+#                         result += 1
+#     print(result)
+
 # 过不了，不知道是为什么
+
+def in_field(a, b, n, m):
+    return 0 <= a <= n - 1 and 0 <= b <= m - 1
+
+
 case_num = int(input().strip())
+neighbor_direction = [(-1, -1), (-1, 0),(-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 for case in range(case_num):
     n, m = [int(i) for i in input().strip().split(" ")]
     mymap = []
@@ -7,98 +118,16 @@ for case in range(case_num):
         mymap.append(list(input().strip()))
     result = 0
     checked = set()
-    if n == 1:
-        if m == 1:
-            if mymap[0][0] == "W":
-                print(1)
-                continue
-            else:
-                print(0)
-                continue
-        else:
-            for j in range(m):
-                if mymap[0][j] == "W":
-                    if j == 0:
-                        neighbors = [mymap[0][1], ]
-                        checked.add((0, 1))
-                    elif j == m - 1:
-                        neighbors = [mymap[0][m - 2], ]
-                        checked.add((0, m - 2))
-                    else:
-                        neighbors = [mymap[0][j - 1], mymap[0][j + 1]]
-                        checked.add((0, j - 1))
-                        checked.add((0, j + 1))
-                    if (0, j) not in checked:
-                        result += 1
-    else:
-        for i in range(n):
-            for j in range(m):
-                if mymap[i][j] == 'W':
-                    neighbors = []
-                    if i == 0:
-                        if j == 0:
-                            neighbors = [mymap[i][j + 1], mymap[i + 1][j], mymap[i + 1][j + 1]]
-                            checked.add((i, j + 1))
-                            checked.add((i + 1, j))
-                            checked.add((i + 1, j + 1))
-                        elif j == m - 1:
-                            neighbors = [mymap[i][j - 1], mymap[i + 1][j], mymap[i + 1][j - 1]]
-                            checked.add((i, j - 1))
-                            checked.add((i + 1, j))
-                            checked.add((i + 1, j - 1))
-                        else:
-                            neighbors = [mymap[i][j + 1], mymap[i][j - 1], mymap[i + 1][j], mymap[i + 1][j - 1], mymap[i + 1][j + 1]]
-                            checked.add((i, j + 1))
-                            checked.add((i, j - 1))
-                            checked.add((i + 1, j + 1))
-                            checked.add((i + 1, j - 1))
-                            checked.add((i + 1, j))
-                    elif i == n - 1:
-                        if j == 0:
-                            neighbors = [mymap[i][j + 1], mymap[i - 1][j], mymap[i - 1][j + 1]]
-                            checked.add((i, j + 1))
-                            checked.add((i - 1, j))
-                            checked.add((i - 1, j + 1))
-                        elif j == m - 1:
-                            neighbors = [mymap[i][j - 1], mymap[i - 1][j], mymap[i - 1][j - 1]]
-                            checked.add((i, j - 1))
-                            checked.add((i - 1, j))
-                            checked.add((i - 1, j - 1))
-                        else:
-                            neighbors = [mymap[i][j + 1], mymap[i][j - 1], mymap[i - 1][j], mymap[i - 1][j - 1],
-                                         mymap[i - 1][j + 1]]
-                            checked.add((i, j + 1))
-                            checked.add((i, j - 1))
-                            checked.add((i - 1, j + 1))
-                            checked.add((i - 1, j - 1))
-                            checked.add((i - 1, j))
-                    else:
-                        if j == 0:
-                            neighbors = [mymap[i][j + 1], mymap[i - 1][j], mymap[i + 1][j], mymap[i - 1][j + 1], mymap[i + 1][j + 1]]
-                            checked.add((i, j + 1))
-                            checked.add((i - 1, j))
-                            checked.add((i + 1, j))
-                            checked.add((i - 1, j + 1))
-                            checked.add((i + 1, j + 1))
-                        elif j == m - 1:
-                            neighbors = [mymap[i][j - 1], mymap[i - 1][j], mymap[i + 1][j], mymap[i - 1][j - 1],
-                                         mymap[i + 1][j - 1]]
-                            checked.add((i, j - 1))
-                            checked.add((i - 1, j))
-                            checked.add((i + 1, j))
-                            checked.add((i - 1, j - 1))
-                            checked.add((i + 1, j - 1))
-                        else:
-                            neighbors = [mymap[i][j - 1], mymap[i - 1][j], mymap[i + 1][j], mymap[i][j + 1], mymap[i - 1][j - 1],
-                                         mymap[i + 1][j - 1], mymap[i + 1][j + 1], mymap[i - 1][j + 1]]
-                            checked.add((i, j - 1))
-                            checked.add((i, j + 1))
-                            checked.add((i - 1, j))
-                            checked.add((i + 1, j))
-                            checked.add((i - 1, j - 1))
-                            checked.add((i - 1, j + 1))
-                            checked.add((i + 1, j - 1))
-                            checked.add((i + 1, j + 1))
-                    if (i, j) not in checked:
-                        result += 1
+    for i in range(n):
+        for j in range(m):
+            if mymap[i][j] == "W":
+                for item in neighbor_direction:
+                    tx = i + item[0]
+                    ty = j + item[1]
+                    if in_field(tx, ty, n, m):
+                        checked.add((tx, ty))
+                if (i, j) not in checked:
+                    result += 1
+                checked.add((i, j))
     print(result)
+
