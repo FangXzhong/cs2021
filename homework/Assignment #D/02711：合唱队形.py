dbd33549dd47c6c217b48e5563a628276000b063
FangXzhong <fangxz2002@outlook.com>
2021-12-16 16:33:58 +0800

    没啥可说的，和登山一模一样
    Signed-off-by: FangXzhong <fangxz2002@outlook.com>

---
+++ homework/Assignment #D/02711：合唱队形.py
@@ -0,0 +1,20 @@
+n = int(input().strip())
+height = [int(i) for i in input().strip().split(" ")]
+height_re = height[::-1]
+up = []
+down = []
+for i in range(len(height)):
+    if i == 0:
+        up.append(1)
+        down.append(1)
+    else:
+        up.append(1)
+        down.append(1)
+        for j in range(i):
+            if height[i] > height[j]:
+                up[-1] = max(up[-1], up[j] + 1)
+            if height_re[i] > height_re[j]:
+                down[-1] = max(down[-1], down[j] + 1)
+down.reverse()
+total = [up[i] + down[i] - 1 for i in range(len(up))]
+print(n - max(total))
